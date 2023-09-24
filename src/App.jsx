7c959398ecac2a0d314aad076e12f05e303e07dc
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-09-24 01:21:18 -0400

    builds useEffect for state and function for attack

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,34 @@ function App() {
   const [currentPlayer, setCurrentPlayer] = useState(1);
   const [message, setMessage] = useState('');
 
+  const randomAttackPoints = () => Math.floor(Math.random() * 10) + 1;
+
+  const attack = () => {
+    if (player1HP <= 0 || player2HP <= 0) {
+      return; // Game over, do not allow further attacks
+    }
+
+    const points = randomAttackPoints();
+
+    if (currentPlayer === 1) {
+      setPlayer2HP(player2HP - points);
+      setCurrentPlayer(2);
+      setMessage(`Player 1 attacked for ${points} points!`);
+    } else {
+      setPlayer1HP(player1HP - points);
+      setCurrentPlayer(1);
+      setMessage(`Player 2 attacked for ${points} points!`);
+    }
+  };
+
+  useEffect(() => {
+    if (player1HP <= 0) {
+      setMessage('Player 1 has lost!');
+    } else if (player2HP <= 0) {
+      setMessage('Player 2 has lost!');
+    }
+  }, [player1HP, player2HP]);
+
 
   return (
     <>
